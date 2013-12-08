06bf91c9c2ffab7b6f0154c0a337fb12b93d8c74
Heviside <heaviside@zoho.com>
2013-12-08 02:48:18 +0100

    Test branching notifications

---
+++ projet_maquette/src/None.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: roma
+ * Date: 02/12/2013
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class None {
+	int testingnotifications;
+}
